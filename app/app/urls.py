eb01a190cd4e4635e9fc54e752ae5091d5f1bc67
Alexis2104 <118292269+Alexis2104@users.noreply.github.com>
2024-09-21 12:25:40 -0600

    Direcciones

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include(('bases.urls', 'bases'), namespace='bases')),
     path('admin/', admin.site.urls),
 ]
