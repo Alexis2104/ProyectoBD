9390138ab115c22521fb1b6a7c4cc1470b08558c
Alexis2104 <118292269+Alexis2104@users.noreply.github.com>
2024-09-21 20:59:52 -0600

    bugs error

--- app/bases/views.py
+++ app/bases/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 from django.views import generic
 
 
+
 class Home(generic.TemplateView):
     template_name = 'base/base.html'
+
+    def get(self, request, *args, **kwargs):
+        print("La vista Home ha sido llamada.")
+        return super().get(request, *args, **kwargs)
