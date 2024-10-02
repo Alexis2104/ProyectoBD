3436ebd9f9d59605b951472aa80e6b42b0dc2579
JxsueMd16 <josuemorandelacruz16@outlook.com>
2024-10-02 00:47:00 -0600

    page

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bases',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-gt'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'America/Guatemala'
 
@@ -123,4 +124,5 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
